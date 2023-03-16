79e38a760f420a08583323a54a6e8f5ab5fe70f6
shineHale <3116227229@qq.com>
2023-03-16 16:47:59 +0800

    fix some faults

--- HUST/hw/lab1/PC.v
+++ HUST/hw/lab1/PC.v
@@ -4,10 +4,11 @@ module PC(D, Clk, Reset, Q);
 // codes
   input [n - 1: 0] D;
   input Clk, Reset;
-  output reg [n - 1: 0] Q;
-  
-  always @(posedge Clk, Reset) begin
-	Q <= D;
-	if (Reset == 1'b1) Q <= 32'b0;
+  output reg [n - 1: 0] Q = 32'bx;
+  always @ (Reset) begin
+  if (Reset == 1'b1) Q <= 32'b0;
+  end
+  always @(posedge Clk) begin
+  if (Reset == 1'b0) Q <= D;
   end
 endmodule
